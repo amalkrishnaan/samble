585b9adfef6dfad3ad963c7d6aabfc4d0521f641
amalkrishnaan <amalkrishnaan@gmail.com>
2024-10-16 13:22:47 +0530

    file added

---
+++ clientside/js/profile.js
@@ -0,0 +1,114 @@
+const url=window.location.href;
+const urlParams=new URLSearchParams(url.split("?")[1]);
+const id=urlParams.get("id");
+
+async function getUser() {
+    const res=await fetch(`http://localhost:3000/api/getuser/${id}`);
+    const user=await res.json();
+    if(user.profile)
+        document.getElementById("profile").src=user.profile;
+    document.getElementById("username").textContent=user.username;
+    document.getElementById("email").textContent=user.email;
+    document.getElementById("place").textContent=user.place;
+    document.getElementById("address").textContent=user.address;
+    document.getElementById("pincode").textContent=user.pincode;
+    document.getElementById("phone").textContent=user.phone;
+    document.getElementById("edit").innerHTML=`<a href="../pages/edit.html?id=${user._id}"><img src="../images/edit.png" alt=""></a>`
+    document.getElementById("delete").innerHTML=`<img src="../images/delete.png" alt="" onclick="deleteUser('${user._id}')">
+`
+}
+getUser();
+
+async function getSProducts() {
+    const res=await fetch(`http://localhost:3000/api/getsproducts/${id}`);
+    const products=await res.json();
+    str=``;
+    console.log(products);
+    products.map((product)=>{
+        str+=`
+            <div class="product">
+                <a href="./products.html?id=${product._id}">
+                    <img src="${product.images[0]}" alt="">
+                    <h3>${product.pname.substring(0,18)}</h3>
+                    <h1 >Rs. ${product.price}</h1>
+                    <p>${product.category.toUpperCase()}</p>
+                </a>
+            </div>
+        `
+    })
+    document.getElementById("products").innerHTML=str
+}
+getSProducts();
+function logout() {
+    localStorage.removeItem("Auth");
+    window.location.href="../pages/signin.html"
+}
+
+document.getElementById("search").addEventListener('keyup',async(e)=>{
+    try {
+        const res=await fetch(`http://localhost:3000/api/getsproducts/${id}`);
+        const products=await res.json();
+        str=``;
+        products.filter((i)=>i.pname.toLowerCase().includes(e.target.value.toLowerCase())).map((product)=>{
+            str+=`
+            <div class="product">
+                <a href="./pages/product.html">
+                    <img src="${product.images[0]}" alt="">
+                    <h3>${product.pname}</h3>
+                    <h1 >Rs. ${product.price}</h1>
+                    <p>${product.description}</p>
+                </a>
+            </div>
+        `
+        })
+
+        document.getElementById("products").innerHTML=str;
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+document.getElementById("filter").addEventListener('change',async(e)=>{
+    try {
+        const res=await fetch(`http://localhost:3000/api/getsproducts/${id}`);
+        const products=await res.json();
+        str=``;
+        products.filter((i)=>i.category.toLowerCase().includes(e.target.value.toLowerCase())).map((product)=>{
+            str+=`
+            <div class="product">
+                <a href="./pages/product.html">
+                    <img src="${product.images[0]}" alt="">
+                    <h3>${product.pname}</h3>
+                    <h1 >Rs. ${product.price}</h1>
+                    <p>${product.description}</p>
+                </a>
+            </div>
+        `
+        })
+        document.getElementById("products").innerHTML=str;
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+async function deleteUser(id) {
+    console.log(id); 
+    if(confirm("Do you want to delete the user?")){
+        await fetch(`http://localhost:3000/api/deleteuser/${id}`,{
+            method:"DELETE"
+        }).then(async(res)=>{
+            const data=await res.json()
+            if(res.status==200){
+                alert(data.msg)
+                getUser();
+                window.location.href="../index.html"
+            } 
+            else{
+                alert("cannot delete the user")
+            }  
+        })
+
+    }
+}
